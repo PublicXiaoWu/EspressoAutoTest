67c2bb1f46f2d63c1045107518452fc44874c96a
lsx <572518791@qq.com>
2018-09-27 14:30:58 +0800

    第一次提交

---
+++ app/src/androidTest/java/com/liushaoxiang/espressoautotest/unit/AllTestSuite.kt
@@ -0,0 +1,6 @@
+package com.liushaoxiang.espressoautotest.unit
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+@RunWith(Suite::class)
+@Suite.SuiteClasses(SettingsActivityTest::class)
+class AllTestSuite {}
